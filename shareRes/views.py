b6c80383f32b640924651ab2e4b9f77fec767d30
inkweon7269 <inkweon7269@gmail.com>
2020-03-23 23:44:22 +0900

    Create category

--- shareRes/views.py
+++ shareRes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
 
 # Create your views here.
 def index(request):
+    categories = Category.objects.all()
+    content = { 'categories': categories }
     # return HttpResponse("index")
-    return render(request, 'shareRes/index.html')
+    return render(request, 'shareRes/index.html', content)
 
 def restaurantDetail(request):
     # return HttpResponse("restaurantDetail")
@@ -14,4 +18,17 @@ def restaurantCreate(request):
     return render(request, 'shareRes/restaurantCreate.html')
 
 def categoryCreate(request):
-    return render(request, 'shareRes/categoryCreate.html')
+    categories = Category.objects.all()
+    content = {'categories': categories}
+    return render(request, 'shareRes/categoryCreate.html', content)
+
+def Create_category(request):
+    category_name = request.POST['categoryName']
+    new_category = Category(category_name = category_name)
+    new_category.save()
+    return HttpResponseRedirect(reverse('index'))
+
+
+
+
+
